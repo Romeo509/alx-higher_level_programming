2c1fda3a4891c5c7510a132ea9c20f46119c8d39
Romeo509 <romeoameku503@gmail.com>
2023-10-30 17:10:44 +0100

    code fixed

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-print(f"Welcome to {str1} {str2}!")
+combined_str = str1 + " " + str2
+message = "Welcome to {}!".format(combined_str)
+print(message)
