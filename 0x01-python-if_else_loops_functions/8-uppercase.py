5b342b4bb9c9ac5ab9a2c92f5f27be6cea5aa344
Romeo509 <romeoameku503@gmail.com>
2023-10-31 23:46:01 +0100

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
