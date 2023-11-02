e7275bcc624cf4cd10873086cf486fe0c73d498e
Romeo509 <romeoameku503@gmail.com>
2023-11-03 00:18:38 +0100

    Right now, its crazy

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import sys
     import hidden_4
-    for name in dir(hidden_4):
-        if not name.startswith("__"):
-            print(name)
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
+
