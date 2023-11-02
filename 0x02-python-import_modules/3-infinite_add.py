2b14bcf48f3208043e4dd40f5a6c678d9cb66ef5
Romeo509 <romeoameku503@gmail.com>
2023-11-02 23:05:11 +0100

    An infinite tricky addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+from sys import argv
 
-    num_args = len(sys.argv) - 1
-    if num_args == 0:
-        print("0")
-    elif num_args > 0:
-        total = sum(int(arg) for arg in sys.argv[1:])
-        print("{}".format(total))
+if __name__ == "__main__":
+   arg_digit = len(argv) - 1
+   if arg_digit == 0:
+      print("{}".format(arg_digit))
+   else:
+      result = []
+      for i in range(1, arg_digit + 1):
+           result.append(int(argv[i]))
+      print("{}".format(sum(result)))
