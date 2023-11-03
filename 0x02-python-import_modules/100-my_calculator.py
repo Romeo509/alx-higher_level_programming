b8ca0931f020324400f522c2ef55bf5b6795ee3d
Romeo509 <romeoameku503@gmail.com>
2023-11-03 01:45:11 +0100

    hope this works now

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
     argv = sys.argv[1:]
-    arg_num = len(argv)
-    if arg_num != 3:
+    arg_digit = len(argv)
+    if arg_digit != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
     elif sys.argv[2] not in '+-*/':
