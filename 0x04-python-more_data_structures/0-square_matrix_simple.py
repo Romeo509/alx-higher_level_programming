7df60c13f74ab0ad8d23a6760ae12a9a5b70818e
Romeo509 <romeoameku503@gmail.com>
2023-11-08 22:37:10 +0100

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     # Create a new matrix using list comprehension with square values
     new_matrix = [[x**2 for x in row] for row in matrix]
