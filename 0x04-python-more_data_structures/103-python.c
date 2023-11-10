b986814f7cd3b3d75ba5a01b978eb96feacb9748
Romeo509 <romeoameku503@gmail.com>
2023-11-11 00:36:04 +0100

    hjdsa

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -13,28 +13,24 @@ char *string;
 long int size, i, limit;
 
 printf("[.] bytes object info\n");
-if (!PyObject_HasAttrString(p, "decode") || !PyBytes_Check(p))
+if (!PyBytes_Check(p))
 {
 printf("  [ERROR] Invalid Bytes Object\n");
 return;
 }
 
-size = PyObject_Size(p);
-string = PyUnicode_DecodeUTF8(PyBytes_AsString(p), size, "strict");
-if (string == NULL)
-{
-printf("  [ERROR] Decode failed\n");
-return;
-}
+size = PyBytes_Size(p);
+string = PyBytes_AsString(p);
 
 printf("  size: %ld\n", size);
 printf("  trying string: %s\n", string);
 
 limit = (size > 10) ? 10 : size;
+
 printf("  first %ld bytes:", limit);
 
 for (i = 0; i < limit; i++)
-printf(" %02x", string[i] & 0xff);
+printf(" %02x", string[i]);
 
 printf("\n");
 }
@@ -50,15 +46,17 @@ void print_python_list(PyObject *p)
 long int size, i;
 PyObject *obj;
 
+size = PyList_Size(p);
+
 printf("[*] Python list info\n");
-printf("[*] Size of the Python List = %ld\n", PyObject_Size(p));
+printf("[*] Size of the Python List = %ld\n", size);
 printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-for (i = 0; i < PyObject_Size(p); i++)
+for (i = 0; i < size; i++)
 {
 obj = PyList_GetItem(p, i);
 printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);
 
-if (PyObject_HasAttrString(obj, "decode") && PyBytes_Check(obj))
+if (PyBytes_Check(obj))
 print_python_bytes(obj);
 }
 }
