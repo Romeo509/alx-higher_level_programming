0551e33eadb3d19be347602ffc2808075f50d281
Romeo509 <romeoameku503@gmail.com>
2023-12-05 00:48:05 +0100

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     Returns True if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class; otherwise False.
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class.
+    """
+    pass
