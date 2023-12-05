1a6bf936b3e134d9d7425bbaa746d858fe1949b0
Romeo509 <romeoameku503@gmail.com>
2023-12-05 03:25:02 +0100

    done

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-Contains the definition of the class Square.
+Definition of the class Square.
 """
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Definition of class Square that inherits from class Rectangle"""
+    """Definition for class Square inheriting from class Rectangle"""
 
     def __init__(self, size):
-        """Initialise an instance of the class Square"""
+        """Initialise an instance"""
         super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
-        """Return the area of the square"""
+        """Return the area of square"""
         return self.__size ** 2
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""
-Contains definition for the class Square
-"""
+"""Definition for the class Square"""
+
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Definition of class square that inherits from class Rectangle"""
+    """Definition of class square"""
 
     def __init__(self, size):
-        """Initializes instance of the class Square"""
+        """Initializes instance """
         super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
-        """Method for computing Square area"""
+        """function for computing Square area"""
         return self.__size ** 2
 
     def __str__(self):
-        """Returns string representation of an instance of class square"""
+        """Returns string representation of an instance"""
         return "[{}] {}/{}".format(type(self).__name, self.__size,
                                    self.__size)
