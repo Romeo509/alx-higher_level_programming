61b2c35a2143c0e6a8867af306ae7ac2970f193e
Romeo509 <romeoameku503@gmail.com>
2023-12-05 23:57:59 +0100

    append funcion updated

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module for append_write function """
+
+
+def append_write(filename="", text=""):
+    """ appends a string at the end of a text file 
+    Args:
+        filename (str): name of the file.
+        text (str): Text to append to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
