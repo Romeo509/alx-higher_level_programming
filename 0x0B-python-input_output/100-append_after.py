0c39070719f2ac64b76f89bb463f985a0925088b
Romeo509 <romeoameku503@gmail.com>
2023-12-06 03:02:32 +0100

    try

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""Defines a text file insertion function."""
+"""function for  a text file insertion function."""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Insert text after each line containing a given string in a file.
-
-    Args:
-        filename (str): The name of the file.
-        search_string (str): The string to search for within the file.
-        new_string (str): The string to insert.
+    """containing a given string in a file.
     """
     text = ""
-    with open(filename) as r:
-        for line in r:
+    with open(filename) as f:
+        for line in f:
             text += line
             if search_string in line:
                 text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+    with open(filename, "w") as a:
+        a.write(text)
