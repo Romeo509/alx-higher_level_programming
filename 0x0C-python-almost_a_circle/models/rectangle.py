b1651b1257b64ce852104dbb8d1eb060f6a3cad9
Romeo509 <romeoameku503@gmail.com>
2023-12-11 15:39:59 +0100

    Add validation for Rectangle class attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,6 @@
 from models.base import Base
 """ rectangle.py """
 
-
 class Rectangle(Base):
     """Rectangle class, inherits from Base."""
 
@@ -22,6 +21,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter for width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -32,6 +35,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter for height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -42,6 +49,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter for x."""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -52,4 +63,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter for y."""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
