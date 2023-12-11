e6741b15efdfb80933db6ea130887e98752e7ab9
Romeo509 <romeoameku503@gmail.com>
2023-12-11 19:03:41 +0100

    Add public getter and setter for size in Square class, with validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,9 +10,19 @@ class Square(Rectangle):
         """Class constructor."""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return the string representation of the square."""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width
         )
-
