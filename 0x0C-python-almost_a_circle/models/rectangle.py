29cacc31d8dc91d4ee666751c4571cff3e605626
Romeo509 <romeoameku503@gmail.com>
2023-12-11 17:32:24 +0100

    Override __str__ method to return [Rectangle] (<id>) <x>/<y> - <width>/<height>

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,9 @@ class Rectangle(Base):
         """Print the rectangle using the character #."""
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Return the string representation of the rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height
+        )
