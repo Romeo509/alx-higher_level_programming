5bc4182e09fca6e7e8e80c31a8c18c9f6480e59c
Romeo509 <romeoameku503@gmail.com>
2023-12-11 17:25:41 +0100

    Add public method display() to print Rectangle instance with # characters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,8 @@ class Rectangle(Base):
     def area(self):
         """Calculate and return the area of the rectangle."""
         return self.__width * self.__height
+
+    def display(self):
+        """Print the rectangle using the character #."""
+        for _ in range(self.__height):
+            print("#" * self.__width)
