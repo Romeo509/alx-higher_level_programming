1d71f946892c7ddb86fccf371aca4a8285171d7d
Romeo509 <romeoameku503@gmail.com>
2024-01-17 23:57:04 +0100

    Task: Updated

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,6 @@
 -- lists all the TV shows contained in hbtn_0d_tvshows that has at least one genre linked
 -- lists all the rows of a database that have one column in common
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,5 @@
--- creating the table unique_id on MySQL server
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+-- script to creates the table unique_id on your MySQL server.
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT UNIQUE DEFAULT 1,
+    name VARCHAR(256)
+);
