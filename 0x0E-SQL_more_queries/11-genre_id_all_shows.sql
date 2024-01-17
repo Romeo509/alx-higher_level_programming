91277b1495f0b0badea96e5caefb1c4655ce6e62
Romeo509 <romeoameku503@gmail.com>
2024-01-18 00:06:33 +0100

    File: Recovered

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- List the shows that have at least one genre linked
+SELECT s.title AS title, g.genre_id AS genre_id
+FROM tv_shows s LEFT OUTER JOIN tv_show_genres g
+  ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
