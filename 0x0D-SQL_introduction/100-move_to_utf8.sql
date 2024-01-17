3b61a7dc77dd9615b0018ff318ef44483db537cb
Romeo509 <romeoameku503@gmail.com>
2024-01-17 12:32:50 +0100

    update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,19 @@
 -- Convert the hbtn_0c_0 database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE
+	hbtn_0c_0
+	CHARACTER SET = utf8mb4
+	COLLATE = utf8mb4_unicode_ci;
 
 -- Convert the first_table table and the name field to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY COLUMN name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE
+	hbtn_0c_0.first_table
+      CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
+
+-- converts the column 'name' to UTF8
+ALTER TABLE
+	hbtn_0c_0.first_table
+	MODIFY name
+	VARCHAR(256)
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
