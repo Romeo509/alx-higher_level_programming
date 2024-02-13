5ca5dd4013c4cdd9ecee0f88c2900b72bd195919
Romeo509 <romeoameku503@gmail.com>
2024-02-13 15:54:04 +0000

    complete

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,12 +4,13 @@ class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       // If width or height is 0 or not a positive integer, create an empty object
-      return {};
+      const emptyObject = {};
+      Object.assign(this, emptyObject);
+    } else {
+      // Initialize instance attributes with the provided values
+      this.width = w;
+      this.height = h;
     }
-
-    // Initialize instance attributes with the provided values
-    this.width = w;
-    this.height = h;
   }
 }
 
