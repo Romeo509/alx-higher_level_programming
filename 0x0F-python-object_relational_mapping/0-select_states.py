6f867983bd02180d30b7122c9a809f0626fac75f
Romeo509 <romeoameku503@gmail.com>
2024-02-16 23:29:32 +0000

    completed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+"""  lists all states from the database  """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    # Get MySQL connection parameters from command line arguments
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Connect to MySQL server
     try:
         db = MySQLdb.connect(
             host="localhost",
@@ -23,7 +21,6 @@ if __name__ == "__main__":
         )
         cursor = db.cursor()
 
-        # Execute SQL query to retrieve and display states
         cursor.execute("SELECT * FROM states ORDER BY id ASC;")
         states = cursor.fetchall()
         for state in states:
@@ -32,6 +29,5 @@ if __name__ == "__main__":
     except MySQLdb.Error as e:
         print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
     finally:
-        # Close the database connection
         if db:
             db.close()
