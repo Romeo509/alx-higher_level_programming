fc0ae56fdaedcdac3c46868f0dfc2730bd0e1705
Romeo509 <romeoameku503@gmail.com>
2024-02-17 23:41:41 +0000

    completed1

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,16 +1,57 @@
 #!/usr/bin/python3
 """
-Script that takes in the name of a state as an argument
-and lists all cities of that state, using the database hbtn_0e_4_usa.
+Lists all cities of a given state from the database
+hbtn_0e_4_usa (SQL injection free).
 """
 
-import sys
 import MySQLdb
+import sys
+
+
+def filter_cities(username, password, database, state_name):
+    """
+    Connects to the MySQL server and displays cities of
+    the given state (SQL injection free).
+
+    :param username: MySQL username
+    :param password: MySQL password
+    :param database: Database name
+    :param state_name: State name to search for
+    """
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+        cursor = db.cursor()
+
+        query = """SELECT cities.name
+                   FROM cities
+                   INNER JOIN states ON states.id = cities.state_id
+                   WHERE states.name = %s"""
+        cursor.execute(query, (state_name,))
+
+        rows = cursor.fetchall()
+        city_names = [row[0] for row in rows]
+        print(", ".join(city_names))
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+    finally:
+        if db:
+            db.close()
+
 
 if __name__ == "__main__":
+    """
+    Main script execution block.
+    """
     if len(sys.argv) != 5:
-        print("""Usage: {} username password database
-              state_name""".format(sys.argv[0]))
+        print("""Usage: {} <username> <password> <database>
+              <state_name>""".format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
@@ -18,29 +59,4 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
-    )
-
-    cursor = db.cursor()
-
-    query = """
-            SELECT GROUP_CONCAT(cities.name SEPARATOR ', ')
-            FROM cities
-            JOIN states ON cities.state_id = states.id
-            WHERE states.name = %s
-            ORDER BY cities.id
-            """
-    cursor.execute(query, (state_name,))
-
-    result = cursor.fetchone()[0]
-    if result:
-        print(result)
-
-    cursor.close()
-    db.close()
+    filter_cities(username, password, database, state_name)
