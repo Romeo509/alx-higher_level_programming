4e1932d70f3d4254e1194fb9be2f7cd6252f6ba7
Romeo509 <romeoameku503@gmail.com>
2024-02-18 16:36:45 +0000

    completed1

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -9,25 +9,22 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    """
-    Main script execution block.
-    """
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        print("Usage: {} username password database".format(sys.argv[0]))
         sys.exit(1)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(username, password, database),
-                           pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, database), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
     new_state = State(name="Louisiana")
+
     session.add(new_state)
     session.commit()
 
