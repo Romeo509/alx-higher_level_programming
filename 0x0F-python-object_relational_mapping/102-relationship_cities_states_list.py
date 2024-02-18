8fe82a4e5bc9b4bbf00be46c3f4f057190ea3b5d
Romeo509 <romeoameku503@gmail.com>
2024-02-18 21:00:29 +0000

    completed1

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -5,33 +5,20 @@ name passed as an argument from the database.
 """
 
 import sys
-from sqlalchemy import create_engine
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State, City
+from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        username, password, database), pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    state_name = sys.argv[4]
-    state = session.query(State).filter_by(name=state_name).first()
-
-    if state:
-        for city in state.cities:
-            print("{}: {} -> {}".format(city.id, city.name, state.name))
-    else:
-        print("State not found in the database")
-
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        for city_ins in instance.cities:
+            print(city_ins.id, city_ins.name, sep=": ", end="")
+            print(" -> " + instance.name)
