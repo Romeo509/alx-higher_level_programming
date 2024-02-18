d8e0e41a0477a14b556ab7954b4751f5cacf6c6f
Romeo509 <romeoameku503@gmail.com>
2024-02-18 17:43:27 +0000

    completed1

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Contains the City class"""
-
+"""Module containing the City class definition."""
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from relationship_state import Base
 
 
 class City(Base):
-    """Class representing the cities table"""
+    """City class that inherits from Base."""
     __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(256), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State")
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,47 @@
 #!/usr/bin/python3
-"""Contains the State class"""
+"""
+Start link class to table in database.
+"""
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from relationship_city import Base, City
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+Base = declarative_base()
 
 
 class State(Base):
-    """Class representing the states table"""
+    """
+    State class that inherits from Base.
+    Represents the 'states' table in the database.
+    """
+
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(256), nullable=False)
+    name = Column(String(128), nullable=False)
     cities = relationship(
-          "City", backref="state", cascade="all, delete-orphan"
+        "City",
+        backref="state",
+        cascade="all, delete-orphan"
     )
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='California')
+    new_city = City(name='San Francisco', state=new_state)
+
+    session.add(new_state)
+    session.add(new_city)
+    session.commit()
+
+    session.close()
