0b0a35e2b3fd108200764edf2fe5d71529c1ade9
Romeo509 <romeoameku503@gmail.com>
2024-02-23 23:27:15 +0000

    complete

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Lists all State objects and corresponding City 
-objects from the database hbtn_0e_101_usa
+Lists all State objects and corresponding City objects
+from the database hbtn_0e_101_usa
 """
 
 import sys
