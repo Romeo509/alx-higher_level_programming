3dfdd5d4c1090ff0d3dc0ae6d18006d9e01d0614
Romeo509 <romeoameku503@gmail.com>
2024-02-29 20:53:12 +0000

    done

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request, and displays the body of the response for a 200 status code
-curl -s -X GET -i "$1" | awk '/HTTP\/1.1 200 OK/{flag=1;next}/HTTP/ && flag{exit}flag' | tail -n +2
+curl -Ls "$1"
