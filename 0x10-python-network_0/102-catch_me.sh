f485b6a75cb7ccf561559d0194f06a781423efc9
Romeo509 <romeoameku503@gmail.com>
2024-02-29 23:06:34 +0000

    done

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me and causes the server to respond with "You got me!"
 curl -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me -L -H "User-Agent:HolbertonSchool" -L -G
-
