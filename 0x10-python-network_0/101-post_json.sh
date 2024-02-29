77b4bdbfcfc97cce2133ba380f28af02fef40cd5
Romeo509 <romeoameku503@gmail.com>
2024-02-29 22:50:09 +0000

    done

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL with the contents of a file as the body
-curl -s "$1" -d "@$2" -H POST -X "Content-Type: application/json"
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
