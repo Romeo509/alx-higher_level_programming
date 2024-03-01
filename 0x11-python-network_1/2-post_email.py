5dfd31dcbda047b720b2609ec5bb5b44ea06f71a
Romeo509 <romeoameku503@gmail.com>
2024-03-01 21:27:48 +0000

    completed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,6 +9,11 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
+    # Check if both URL and email are provided
+    if len(sys.argv) != 3:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        sys.exit(1)
+
     url = sys.argv[1]
     email = sys.argv[2]
 
