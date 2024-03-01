bb950137e7c14743044b707aae70a0db4713ae55
Romeo509 <romeoameku503@gmail.com>
2024-03-01 21:54:24 +0000

    fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,6 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    # Check if both URL and email are provided
     if len(sys.argv) != 3:
         print("Usage: {} <URL> <email>".format(sys.argv[0]))
         sys.exit(1)
@@ -17,11 +16,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    # Data to be sent in the POST request
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode('ascii')
 
-    # Make the POST request
-    with urllib.request.urlopen(url, data=data) as response:
-        body = response.read().decode('utf-8')
-        print("Your email is: {}".format(email))
-        print(body)
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
