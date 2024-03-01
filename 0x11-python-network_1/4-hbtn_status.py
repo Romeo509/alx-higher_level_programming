999821d65eda1d65b9b4bb830698a098f9f2d16a
Romeo509 <romeoameku503@gmail.com>
2024-03-01 22:30:18 +0000

    fixed

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+using requests
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
