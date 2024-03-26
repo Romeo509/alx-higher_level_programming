c0819e223c4cfd6817e6ead5c919c83841c102b4
Romeo509 <romeoameku503@gmail.com>
2024-03-26 14:19:42 +0000

    Read and print file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-
-if (process.argv.length !== 3) {
-    console.error(new Error('Incorreect number of arguments'));
-    process.exit(1);
-}
-
-const [, , filePath] = process.argv;
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading file: ${err.message}');
-        return;
-    }
-
+const argv = process.argv;
+let fs = require('fs');
+fs.readFile(argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
     console.log(data);
+  }
 });
