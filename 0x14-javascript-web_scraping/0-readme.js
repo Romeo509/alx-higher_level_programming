ad6d2a4757d41a6f91f5438ddde8db41879a95e8
Romeo509 <romeoameku503@gmail.com>
2024-03-26 14:02:06 +0000

    Read and write file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,24 +3,24 @@
 const fs = require('fs');
 
 if (process.argv.length !== 3) {
-    console.error(new Error('Incorrect number of arguments'));
-    process.exit(1);
+  console.error(new Error('Incorrect number of arguments'));
+  process.exit(1);
 }
 
-const filePath = process.argv[2];
+const [, , filePath] = process.argv;
 
 fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Error reading file: ${err.message}`);
+    return;
+  }
+
+  console.log(data);
+
+  fs.writeFile(filePath, data, 'utf8', (err) => {
     if (err) {
-        console.error('Error reading file:', err.message);
-        return;
+      console.error(`Error writing file: ${err}`);
+      return;
     }
-
-    console.log(data);
-
-    fs.writeFile(filePath, data, 'utf8', (err) => {
-        if (err) {
-            console.error('Error writing file:', err);
-            return;
-        }
-    });
+  });
 });
