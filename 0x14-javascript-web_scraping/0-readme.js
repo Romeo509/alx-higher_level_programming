6ae12d43048307d586a4f0c26b4a025651dd6921
Romeo509 <romeoameku503@gmail.com>
2024-03-26 23:20:09 +0000

    rewrite script with correct semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const argv = process.argv;
-let fs = require('fs');
+const fs = require('fs');
 fs.readFile(argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,13 +5,12 @@ const fs = require('fs');
 const [, , filePath, content] = process.argv;
 
 if (typeof content !== 'string') {
-    console.error(new Error('Content must be a string'));
-    process.exit(1);
+  console.error(new Error('Content must be a string'));
+  process.exit(1);
 }
 
 fs.writeFile(filePath, content, 'utf8', (err) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+  if (err) {
+    console.error(err);
+  }
 });
