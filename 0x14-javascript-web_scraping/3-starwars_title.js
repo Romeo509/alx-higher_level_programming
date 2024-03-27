2d7d54dfae06495971ebc3550a0a9cd7b9bcc1c9
Romeo509 <romeoameku503@gmail.com>
2024-03-27 02:54:41 +0000

    rewrite script with correct semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,21 @@ const url = 'http://swapi.co/api/films/';
 const argv = process.argv;
 const request = require('request');
 
-request(url + argv[2], function (err, res, body) {
-  if (err) {
-    console.log(err);
-  }
-  const json = JSON.parse(body);
-  console.log(json.title);
-});
+try {
+    request(url + argv[2], function (err, res, body) {
+        if (err) {
+            console.error('Error occurred:', err);
+            process.exit(1);
+        }
+        try {
+            const json = JSON.parse(body);
+            console.log(json.title);
+        } catch (parseError) {
+            console.error('Error parsing JSON:', parseError);
+            process.exit(1);
+        }
+    });
+} catch (requestError) {
+    console.error('Error making request:', requestError);
+    process.exit(1);
+}
