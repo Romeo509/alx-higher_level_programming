3224e63302f4b0845a28cc30b81d9341cbb614c5
Romeo509 <romeoameku503@gmail.com>
2024-03-27 03:08:41 +0000

    rewrite script with correct semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,24 +1,26 @@
 #!/usr/bin/node
 
-const url = 'http://swapi.co/api/films/';
-const argv = process.argv;
 const request = require('request');
 
-try {
-  request(url + argv[2], function (err, res, body) {
-    if (err) {
-      console.error('Error occurred:', err);
-      process.exit(1);
-    }
-    try {
-      const json = JSON.parse(body);
-      console.log(json.title);
-    } catch (parseError) {
-      console.error('Error parsing JSON:', parseError);
-      process.exit(1);
-    }
-  });
-} catch (requestError) {
-  console.error('Error making request:', requestError);
+// Check if the correct number of arguments is provided
+if (process.argv.length !== 3) {
+  console.error(new Error('Usage: ./3-starwars_title.js <movie_id>'));
   process.exit(1);
 }
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make a GET request to the Star Wars API to fetch movie details
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  // Parse the response body to JSON
+  const movie = JSON.parse(body);
+
+  // Print the title of the movie
+  console.log(movie.title);
+});
