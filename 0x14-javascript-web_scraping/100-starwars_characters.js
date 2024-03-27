214c3e0adc03d5c0864b13249cc6c5a710373d3b
Romeo509 <romeoameku503@gmail.com>
2024-03-27 02:56:13 +0000

    rewrite script with correct semistandard

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,8 +4,8 @@ const request = require('request');
 
 // Check if the correct number of arguments is provided
 if (process.argv.length !== 3) {
-    console.error(new Error('Usage: ./100-starwars_characters.js <movie_id>'));
-    process.exit(1);
+  console.error(new Error('Usage: ./100-starwars_characters.js <movie_id>'));
+  process.exit(1);
 }
 
 const movieId = process.argv[2];
@@ -13,23 +13,23 @@ const url = `https://swapi.dev/api/films/${movieId}/`;
 
 // Make a GET request to the API URL
 request.get(url, (error, response, body) => {
-    if (error) {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  // Parse the response body to JSON
+  const film = JSON.parse(body);
+
+  // Print characters
+  film.characters.forEach(characterUrl => {
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
         console.error(error);
-        process.exit(1);
-    }
-
-    // Parse the response body to JSON
-    const film = JSON.parse(body);
-
-    // Print characters
-    film.characters.forEach(characterUrl => {
-        request.get(characterUrl, (error, response, body) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-            const character = JSON.parse(body);
-            console.log(character.name);
-        });
+        return;
+      }
+      const character = JSON.parse(body);
+      console.log(character.name);
     });
+  });
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,20 +5,20 @@ const argv = process.argv;
 const request = require('request');
 
 try {
-    request(url + argv[2], function (err, res, body) {
-        if (err) {
-            console.error('Error occurred:', err);
-            process.exit(1);
-        }
-        try {
-            const json = JSON.parse(body);
-            console.log(json.title);
-        } catch (parseError) {
-            console.error('Error parsing JSON:', parseError);
-            process.exit(1);
-        }
-    });
+  request(url + argv[2], function (err, res, body) {
+    if (err) {
+      console.error('Error occurred:', err);
+      process.exit(1);
+    }
+    try {
+      const json = JSON.parse(body);
+      console.log(json.title);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+      process.exit(1);
+    }
+  });
 } catch (requestError) {
-    console.error('Error making request:', requestError);
-    process.exit(1);
+  console.error('Error making request:', requestError);
+  process.exit(1);
 }
